5077c4c055889d01014b32227369784c3aefd9a5
Wanbok Choi <choi@wanbok.com>
2012-10-06 15:57:50 +0900

    removing execjs

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-WanbokCom::Application.config.session_store :cookie_store, key: '_wanbok.com_session'
+WanbokCom::Application.config.session_store :cookie_store, key: '234KSJDFKJ#2JKSDF#'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
