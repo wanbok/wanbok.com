60fff06fc6bc52f0c036b70b7ff331e0c6f824a2
Wanbok Choi <choi@wanbok.com>
2012-10-06 16:00:15 +0900

    remove initiailizing

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-WanbokCom::Application.initialize!
+# WanbokCom::Application.initialize!
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-WanbokCom::Application.config.session_store :cookie_store, key: '234KSJDFKJ#2JKSDF#'
+WanbokCom::Application.config.session_store :cookie_store, key: '_wanbok.com_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
